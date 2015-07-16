3df19901e8e409f4ba0c89f7bc6a291dbb08fa76
baurine <2008.hbl@gmail.com>
2015-07-16 23:45:14 +0800

    do palindrome number

---
+++ c++/palindrome_number.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+
+        int y = 0;
+        int z = x;
+        while (x > 0) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return y == z;
+    }
+};
