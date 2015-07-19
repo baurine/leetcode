4284942057d792d69d5e431f833cc0206887d940
baurine <2008.hbl@gmail.com>
2015-07-19 15:14:16 +0800

    Implement Stack using Queues accepted

---
+++ c++/implement_stack_using_queues.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/implement-stack-using-queues/
+
+class Stack {
+private:
+    list<int> queue_list;
+public:
+    // Push element x onto stack.
+    void push(int x) {
+        int size = queue_list.size();
+        queue_list.push_back(x);
+        while (size-- > 0) {
+            queue_list.push_back(queue_list.front());
+            queue_list.pop_front();
+        }
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        queue_list.pop_front();
+    }
+
+    // Get the top element.
+    int top() {
+        return queue_list.front();
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return queue_list.empty();
+    }
+};
