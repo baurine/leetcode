c0ef4bfd69e1bd53062a51f8d30b19953d069271
baurine <2008.hbl@gmail.com>
2020-02-07 19:24:48 +0800

    resolve exercism rust track: binary search solution 2

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+fn find_1(array: &[i32], key: i32) -> Option<usize> {
     if array.is_empty() {
         return None;
     }
@@ -17,4 +17,48 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
         }
     }
     None
+}
+
+fn find_2(array: &[i32], key: i32) -> Option<usize> {
+    let mut s = &array[..];
+    let mut idx_base = 0;
+    while !s.is_empty() {
+        let middle_idx = s.len() / 2;
+        let value = s.get(middle_idx).unwrap();
+        if value == &key {
+            return Some(idx_base + middle_idx);
+        }
+        if value < &key {
+            s = &s[middle_idx + 1..];
+            idx_base += middle_idx + 1;
+        } else {
+            s = &s[0..middle_idx];
+        }
+    }
+    None
+}
+
+fn find_3<T: PartialOrd>(array: &[T], key: T) -> Option<usize> {
+    let mut s = &array[..];
+    let mut idx_base = 0;
+    while !s.is_empty() {
+        let middle_idx = s.len() / 2;
+        let value = s.get(middle_idx).unwrap();
+        if value == &key {
+            return Some(idx_base + middle_idx);
+        }
+        if value < &key {
+            s = &s[middle_idx + 1..];
+            idx_base += middle_idx + 1;
+        } else {
+            s = &s[0..middle_idx];
+        }
+    }
+    None
+}
+
+pub fn find(array: &[i32], key: i32) -> Option<usize> {
+    find_1(array, key);
+    find_2(array, key);
+    find_3(array, key)
 }
