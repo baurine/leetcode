be17993f7c1818361d9d594230b08f49c77b0b2b
baurine <2008.hbl@gmail.com>
2020-02-07 18:54:37 +0800

    resolve exercism rust track: binary search

--- exercism/rust/binary-search/src/lib.rs
+++ exercism/rust/binary-search/src/lib.rs
@@ -1,7 +1,20 @@
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    if array.is_empty() {
+        return None;
+    }
+    let mut start: i32 = 0;
+    let mut end: i32 = (array.len() - 1) as i32;
+    while start <= end {
+        let middle_idx = (start + end) / 2;
+        let value = array.get(middle_idx as usize).unwrap();
+        if value == &key {
+            return Some(middle_idx as usize);
+        }
+        if value < &key {
+            start = middle_idx + 1;
+        } else {
+            end = middle_idx - 1;
+        }
+    }
+    None
 }
