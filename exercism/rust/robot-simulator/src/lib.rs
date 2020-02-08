aa0ea17cc969a9bafdde93290e5ee2b88cbe5645
baurine <2008.hbl@gmail.com>
2020-02-08 18:15:15 +0800

    resolve exercism rust track: robot simulator

--- exercism/rust/robot-simulator/src/lib.rs
+++ exercism/rust/robot-simulator/src/lib.rs
@@ -1,6 +1,3 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(PartialEq, Debug)]
 pub enum Direction {
     North,
@@ -9,37 +6,67 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+    x: i32,
+    y: i32,
+    d: Direction,
+}
 
 impl Robot {
     pub fn new(x: i32, y: i32, d: Direction) -> Self {
-        unimplemented!("Create a robot at (x, y) ({}, {}) facing {:?}", x, y, d,)
+        Robot { x, y, d }
     }
 
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        let next_d = match self.d {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        };
+        Robot { d: next_d, ..self }
     }
 
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        let next_d = match self.d {
+            Direction::North => Direction::West,
+            Direction::East => Direction::North,
+            Direction::South => Direction::East,
+            Direction::West => Direction::South,
+        };
+        Robot { d: next_d, ..self }
     }
 
     pub fn advance(self) -> Self {
-        unimplemented!()
+        let mut next_x = self.x;
+        let mut next_y = self.y;
+        match self.d {
+            Direction::North => next_y += 1,
+            Direction::East => next_x += 1,
+            Direction::South => next_y -= 1,
+            Direction::West => next_x -= 1,
+        };
+        Robot {
+            x: next_x,
+            y: next_y,
+            ..self
+        }
     }
 
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!(
-            "Follow the given sequence of instructions: {}",
-            instructions
-        )
+        instructions.chars().fold(self, |accu, c| match c {
+            'R' => accu.turn_right(),
+            'L' => accu.turn_left(),
+            'A' => accu.advance(),
+            _ => accu,
+        })
     }
 
     pub fn position(&self) -> (i32, i32) {
-        unimplemented!()
+        (self.x, self.y)
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.d
     }
 }
