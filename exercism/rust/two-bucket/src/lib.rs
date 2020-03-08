af59dc53d57c8c44afdeb4561cc94be79111454c
baurine <2008.hbl@gmail.com>
2020-03-08 22:31:35 +0800

    resolve exercism rust track: two  bucket

--- exercism/rust/two-bucket/src/lib.rs
+++ exercism/rust/two-bucket/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
@@ -16,13 +18,83 @@ pub struct BucketStats {
     pub other_bucket: u8,
 }
 
+pub fn is_solvable(cap_1: u8, cap_2: u8, goal: u8) -> bool {
+    if cap_1 == goal || cap_2 == goal {
+        return true;
+    }
+    let mut remains: HashSet<u8> = HashSet::new();
+    for i in 1.. {
+        let remain = i * cap_1 % cap_2;
+        if remain == goal {
+            return true;
+        }
+        if remains.contains(&remain) {
+            return false;
+        }
+        remains.insert(remain);
+    }
+    false
+}
+
+pub fn ext_euclid(cap_1: u8, cap_2: u8, goal: u8) -> (u8, u8, u8) {
+    let mut moves = 0_u8;
+    let mut cur_1 = 0_u8;
+    let mut cur_2 = 0_u8;
+    let (goal_cap, other_liter) = loop {
+        if cur_1 == 0 {
+            cur_1 = cap_1;
+            moves += 1;
+        } else if cur_2 == 0 && cap_2 == goal {
+            cur_2 = cap_2;
+            moves += 1;
+        } else if cur_1 < cap_2 - cur_2 {
+            cur_2 += cur_1;
+            cur_1 = 0;
+            moves += 1;
+        } else {
+            cur_1 = cur_1 - (cap_2 - cur_2);
+            cur_2 = cap_2;
+            moves += 1;
+        }
+        if cur_1 == goal {
+            break (cap_1, cur_2);
+        }
+        if cur_2 == goal {
+            break (cap_2, cur_1);
+        }
+        if cur_2 == cap_2 {
+            cur_2 = 0;
+            moves += 1;
+        }
+    };
+    (moves, goal_cap, other_liter)
+}
+
 /// Solve the bucket problem
-pub fn solve(capacity_1: u8, capacity_2: u8, goal: u8, start_bucket: &Bucket) -> Option<BucketStats> {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}, or None if impossible",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+pub fn solve(
+    capacity_1: u8,
+    capacity_2: u8,
+    goal: u8,
+    start_bucket: &Bucket,
+) -> Option<BucketStats> {
+    if start_bucket == &Bucket::One && !is_solvable(capacity_1, capacity_2, goal) {
+        return None;
+    }
+    if start_bucket == &Bucket::Two && !is_solvable(capacity_2, capacity_1, goal) {
+        return None;
+    }
+    let (moves, goal_cap, other_liter) = if start_bucket == &Bucket::One {
+        ext_euclid(capacity_1, capacity_2, goal)
+    } else {
+        ext_euclid(capacity_2, capacity_1, goal)
+    };
+    Some(BucketStats {
+        moves,
+        goal_bucket: if goal_cap == capacity_1 {
+            Bucket::One
+        } else {
+            Bucket::Two
+        },
+        other_bucket: other_liter,
+    })
 }
