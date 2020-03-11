8197dcf7ef8cf42ad2234bfa713c5ffc952212a8
baurine <2008.hbl@gmail.com>
2020-03-11 12:28:57 +0800

    resolve exercism rust track: palindrome products

--- exercism/rust/palindrome-products/src/lib.rs
+++ exercism/rust/palindrome-products/src/lib.rs
@@ -1,26 +1,77 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Palindrome {
-    // implement your palindrome type here
+    products: Vec<(u64, u64)>,
+    val: u64,
 }
 
 impl Palindrome {
     pub fn new(a: u64, b: u64) -> Palindrome {
-        unimplemented!("create a palindrome with factors ({}, {})", a, b)
+        Palindrome {
+            products: vec![(a, b)],
+            val: a * b,
+        }
     }
 
     pub fn value(&self) -> u64 {
-        unimplemented!("return the value of this palindrome")
+        self.val
     }
 
     pub fn insert(&mut self, a: u64, b: u64) {
-        unimplemented!("insert new factors ({}, {}) into this palindrome", a, b)
+        if a * b == self.val {
+            self.products.push((a, b))
+        }
     }
 }
 
+fn is_palindrome(n: u64) -> bool {
+    n.to_string().chars().rev().collect::<String>() == n.to_string()
+}
+
 pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome)> {
-    unimplemented!(
-        "Find the min and max palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    if min > max {
+        return None;
+    }
+
+    // cache
+    let mut num_palindrome: HashMap<u64, bool> = HashMap::new();
+
+    let mut min_palindrome: Option<Palindrome> = None;
+    let mut max_palindrome: Option<Palindrome> = None;
+    for i in min..=max {
+        for j in i..=max {
+            let v = i * j;
+
+            let palindrome = match num_palindrome.get(&v) {
+                None => {
+                    let b = is_palindrome(v);
+                    num_palindrome.insert(v, b);
+                    b
+                }
+                Some(b) => *b,
+            };
+            if !palindrome {
+                continue;
+            }
+
+            match &mut min_palindrome {
+                None => min_palindrome = Some(Palindrome::new(i, j)),
+                Some(ref p) if v < p.value() => min_palindrome = Some(Palindrome::new(i, j)),
+                Some(ref mut p) if v == p.value() => p.insert(i, j),
+                _ => (),
+            }
+            match &mut max_palindrome {
+                None => max_palindrome = Some(Palindrome::new(i, j)),
+                Some(ref p) if v > p.value() => max_palindrome = Some(Palindrome::new(i, j)),
+                Some(ref mut p) if v == p.value() => p.insert(i, j),
+                _ => (),
+            }
+        }
+    }
+
+    match (min_palindrome, max_palindrome) {
+        (Some(min), Some(max)) => Some((min, max)),
+        (_, _) => None,
+    }
 }
